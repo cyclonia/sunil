0c5d8b1b1cce4e62500200331f5360b49b2f6b4d
cyclonia <myworldsunil@gmail.com>
2020-12-21 09:48:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>My React App.</h1>
       <h2>Author : Sunil Sharma</h2>
       <h3>I have just started the project, will complete this by end of 2021.</h3>
-      <img src={profileImage} alt="profile-pic"/>
+      
     </div>
   );
 }
